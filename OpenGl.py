0d7a352cedeee11ce6120983adc555d8c3028c29
romainTily <32135088+romainTily@users.noreply.github.com>
2017-10-10 09:56:54 +0200

    Add files via upload

--- OpenGl.py
+++ OpenGl.py
@@ -10,7 +10,7 @@ class BASIC():
 		self.indSample = []
 
 		self.initCONST()
-		self.initScreen()
+		self.firstRun()
 
 		self.firstSorter()
 
@@ -18,14 +18,16 @@ class BASIC():
 
 	def ghettoLog(self,data):
 		with open("log.txt","a+") as f:
-			f.write(data+"\n")
+			f.write(str(data)+"\n")
 
 
 	def initCONST(self):
 		self.HEIGHT = 640
 		self.WIDTH = 1300
 
-		self.SAMPLE_NUMBER = 1
+		self.SAMPLE_NUMBER = 6
+
+		self.baseWidth = math.floor(self.WIDTH/((self.SAMPLE_NUMBER*2)+1))
 
 		self.color = {'white' : (255,255,255),
 					  'light blue' : (25,201,255),
@@ -34,36 +36,36 @@ class BASIC():
 
 
 	def drawCursor(self,ind):
-		ind+=1 ; 
-
-		baseWidth = self.WIDTH/(self.SAMPLE_NUMBER*2)
-
-		ecart = baseWidth*ind
+		ecart = self.baseWidth*(ind*2)
 
 		#cursor
-		pygame.draw.polygon(self.dsp,self.color["white"],[[ecart+ind*(baseWidth*1.5),620],[ecart+ind*(baseWidth*1.2),630],[ecart+ind* (baseWidth*1.8) ,630]])
+		pygame.draw.polygon(self.dsp, self.color["white"], [ [ecart+(self.baseWidth*1.5),620], 
+															 [ecart+(self.baseWidth*1.4),630], 
+															 [ecart+(self.baseWidth*1.6),630]])
 		
 		#+ cursor outline (no outline)
 		# pygame.draw.polygon(self.dsp,self.color["light blue"],[[ind*(baseWidth*1.5),620],[ind*baseWidth,630],[ind*(baseWidth*2),630]],1)
 
 
-	def drawSamples(self,heightList):
-		width = self.WIDTH/(self.SAMPLE_NUMBER*2)
+	def drawSamples(self, heightList, selected=[]):
 		startY = self.HEIGHT-40 ; ecart = 0
 
 		for i, height in enumerate(heightList):
-			ecart += width
-			self.drawOne(i,width,ecart,height,startY)
+			ecart += self.baseWidth
+
+			color = self.color['grey'] if i in selected else self.color['black']
+
+			self.drawOne(i,self.baseWidth,ecart,height,startY,color)
 
 
-	def drawOne(self, n, width, ecart, height,startY):
-		pygame.draw.rect(self.dsp, self.color['black'],(width*n+ecart, startY, width, -height))
+	def drawOne(self, n, width, ecart, height,startY,color):
+		pygame.draw.rect(self.dsp, color, ((width*n)+ecart, startY, width, -height))
 
 		#the outline fuck my adaptative layout, gush
 		# pygame.draw.rect(self.dsp, self.color['grey'],(width*n+ecart, startY, width, -height), 3)
 
 
-	def initScreen(self):
+	def firstRun(self):
 		pygame.init()
 
 		self.dsp = pygame.display.set_mode((self.WIDTH,self.HEIGHT))
@@ -76,7 +78,7 @@ class BASIC():
 		self.drawCursor(0)
 
 		#first random render, rect are randomly ordered (some value are hard coded)
-		firstRandom = [i for i in range(math.floor(300/self.SAMPLE_NUMBER*2), self.HEIGHT-40,math.floor(300/self.SAMPLE_NUMBER*2))]
+		firstRandom = [i for i in range(math.floor(300/self.SAMPLE_NUMBER*2), self.HEIGHT, math.floor(300/self.SAMPLE_NUMBER*2))]
 		random.shuffle(firstRandom)
 
 		#then draw them
@@ -85,14 +87,24 @@ class BASIC():
 		#store them in the ind list
 		self.indSample.extend(firstRandom)
 
+		self.ghettoLog(self.indSample)
 
-	def nextFrame():
-		pass
+
+	def nextFrame(self):
+		self.dsp.fill(self.color["white"])
+
+
+
+		#redraw the rect
+		pygame.draw.rect(self.dsp,self.color["black"],(0,self.HEIGHT,self.WIDTH,-39))
+
+
 
 
 	def firstSorter(self):
 		while True:
 
+			self.nextFrame()
 			for event in pygame.event.get():
 				if event.type == QUIT:
 					pygame.quit()
